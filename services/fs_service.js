c08db2d95d086d82254c8c8000e75600a8f62780
DorukGercel <dorukgercel@hotmail.com>
2021-12-05 11:49:48 +0300

    Create helper services

---
+++ services/fs_service.js
@@ -0,0 +1,8 @@
+const fs = require("fs")
+
+module.exports = {
+    log : (req, reqTime) => {
+        const data = `T:${reqTime} M:${req.method} P:${req.url} C:${req.socket.remoteAddress}\n`;
+        fs.appendFile("log.txt", data, () => console.log("Request logged"));
+    }
+}
---
+++ services/library_service.js
@@ -0,0 +1,49 @@
+const library = {
+    nextId: 3,
+    books: [{
+            id: 1,
+            name: "Sherlock Holmes",
+            author: "Arthur Doyle"
+        },{
+            id: 2,
+            name: "Sherlock Holmes 2",
+            author: "Arthur Doyle"
+        }
+    ]
+}
+
+const findBook = (name) => {
+    for(let book of library.books) {
+        if(book.name === name) {
+            return book;
+        }
+    }
+    return null;
+}
+
+module.exports = {
+    /* GET all books. */
+    getAll: () => {
+        return library.books
+    },
+    /* POST a book. */
+    addBook: (bookReq) => {
+        if(findBook(bookReq.name)) {return;}
+        library.books.push({
+            id: library.nextId,
+            name: bookReq.name,
+            author: bookReq.author
+        });
+        library.nextId += 1;
+    },
+    /* PUT a book. */
+    modifyBook: (bookReq) => {
+        let book =  findBook(bookReq.name);
+        if(!book) {return;}
+        book.author = bookReq.author
+    },
+    /* DELETE a book. */
+    deleteBook: (bookReq) => {
+        library.books = library.books.filter((book) => {return book.name !== bookReq.name})
+    }
+}
